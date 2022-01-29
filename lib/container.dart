c24edcb01a977422f2da0ab5f3c37b3b3aade56c
Jmsil <jmsilva.inbox@gmail.com>
2022-01-29 17:58:16 -0300

    Removes the old CvContainer

--- lib/container.dart
+++ lib/container.dart
@@ -1,24 +1,5 @@
 import 'package:flutter/material.dart';
 
-class CvContainer extends AspectRatio {
-  CvContainer(double borderSize, double radius, String? asset, Widget? child)
-    :
-    super(
-      aspectRatio: 1,
-      child: Container(
-        decoration: ShapeDecoration(
-          shape: BeveledRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(radius)),
-            side: BorderSide(width: borderSize / 2, color: Colors.blueGrey[100]!)
-          ),
-          image: asset == null
-            ? null : DecorationImage(fit: BoxFit.cover, image: AssetImage('assets/$asset'))
-        ),
-        child: child
-      )
-    );
-}
-
 class AppContainer extends StatelessWidget {
   static const BorderRadius defaultRadius = BorderRadius.all(Radius.circular(8));
 
