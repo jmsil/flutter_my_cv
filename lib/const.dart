8c7451947ecd7504061e700e77b8865d8b3ae787
Jmsil <jmsilva.inbox@gmail.com>
2022-01-30 18:03:03 -0300

    Creates the UI constants class

---
+++ lib/const.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppUiConst {
+  AppUiConst._();
+
+  static const SizedBox hsep8 = SizedBox(width: 8);
+  static const SizedBox vsep8 = SizedBox(height: 8);
+  static const SizedBox vsep12 = SizedBox(height: 12);
+  static const SizedBox vsep16 = SizedBox(height: 16);
+  static const SizedBox vsep40 = SizedBox(height: 40);
+}
+
+class AppIcons {
+  AppIcons._();
+
+  static const IconData account = Icons.account_circle_outlined;
+  static const IconData arrow = Icons.arrow_right;
+  static const IconData availability = Icons.how_to_reg_outlined;
+  static const IconData back = Icons.arrow_back;
+  static const IconData code = Icons.code;
+  static const IconData course = Icons.card_membership_outlined;
+  static const IconData education = Icons.school_outlined;
+  static const IconData experience = Icons.business_center_outlined;
+  static const IconData language = Icons.language_outlined;
+  static const IconData link = Icons.link;
+  static const IconData local = Icons.room_outlined;
+  static const IconData mail = Icons.mail_outlined;
+  static const IconData phone = Icons.phone_outlined;
+  static const IconData summary = Icons.summarize_outlined;
+}
